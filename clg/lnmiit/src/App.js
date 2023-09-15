c5843392308385f6cc30ed9a74afe66360eda0b3
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-09-15 21:07:59 +0530

    Created files

---
+++ clg/lnmiit/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+        <Route path="/" element={<Navbar />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
